62a3d0f2a22fd6bfa2c13417b11027933a8f3a97
Marcin Koba <mrcnkoba@gmail.com>
2018-07-16 16:26:27 +0200

    Updated NPM package in React example.

--- react/src/index.js
+++ react/src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Talk from 'talkjs';
+import * as Talk from 'talkjs';
 
 class App extends Component {
     constructor(props) {
