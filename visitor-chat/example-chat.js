8d96aba7f668799dd9168c2c2b89bda7a955705a
Ashley Boxshall <ashley@talkjs.com>
2021-01-22 10:30:22 +0000

    fix(example): updated snippet version

--- visitor-chat/example-chat.js
+++ visitor-chat/example-chat.js
@@ -1,7 +1,7 @@
- (function(t,a,l,k,j,s){
-    s=a.createElement('script');s.async=1;s.src="https://cdn.talkjs.com/talk.js";a.head.appendChild(s)
-    ;k=t.Promise;t.Talk={v:1,ready:{then:function(f){if(k)return new k(function(r,e){l.push([f,r,e])});l
-    .push([f])},catch:function(){return k&&new k()},c:l}};})(window,document,[]);
+(function(t,a,l,k,j,s){
+s=a.createElement('script');s.async=1;s.src="https://cdn.talkjs.com/talk.js";a.head.appendChild(s)
+;k=t.Promise;t.Talk={v:3,ready:{then:function(f){if(k)return new k(function(r,e){l.push([f,r,e])});l
+.push([f])},catch:function(){return k&&new k()},c:l}};})(window,document,[]);
 
 Talk.ready.then(function () {
     
