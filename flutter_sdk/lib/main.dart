a19b001c22d212f6ae08b4ad61228ff2daa41888
Franco Bugnano <franco@talkjs.com>
2022-02-02 15:15:06 +0100

    Updated Flutter SDK example

--- flutter_sdk/lib/main.dart
+++ flutter_sdk/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:talkjs/talkjs.dart';
+import 'package:talkjs_flutter/talkjs_flutter.dart';
 
 /// The object containing the state of the app
 class MyAppState with ChangeNotifier {
