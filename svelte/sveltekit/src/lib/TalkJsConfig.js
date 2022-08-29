328ce256dd6950c23f5bd2a8583e69d60710dc96
steven <github@stevenwaterman.uk>
2022-08-29 08:33:56 +0100

    (SvelteKit) Remove unneeded users from TalkJsConfig

--- svelte/sveltekit/src/lib/TalkJsConfig.js
+++ svelte/sveltekit/src/lib/TalkJsConfig.js
@@ -17,21 +17,3 @@ export let abby = {
   welcomeMessage: 'Hello, how are you today?',
   role: 'default',
 };
-
-export let rebecca = {
-  id: "54321",
-  name: "Rebecca Pattison",
-  email: "rebecca.pattison@example.com",
-  photoUrl: "https://talkjs.com/images/avatar-2.jpg",
-  welcomeMessage: "Hey there! Love to chat :-)",
-  role: 'default'
-}
-
-export let george = {
-  id: "12345231",
-  name: "George Owens",
-  email: "george.owens@example.com",
-  photoUrl: "https://talkjs.com/images/avatar-5.jpg",
-  welcomeMessage: "Hey there! How are you? :D",
-  role: 'default'
-}
