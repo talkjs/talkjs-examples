7ae9849dac8c3bc029d791f8a27d43f2488c0c20
Mario Mucalo <mucalo.mario@gmail.com>
2023-03-18 08:47:18 +0100

    Updated data model with additional properties

--- blazor/blazor-wasm-basic-example/Models/TalkJsModels/User.cs
+++ blazor/blazor-wasm-basic-example/Models/TalkJsModels/User.cs
@@ -4,8 +4,12 @@
     {
         public long Id { get; set; }
         public  string Name { get; set; }
-        public string Email { get; set; }
+        public string[] Email { get; set; }
+        public string[] Phone { get; set; }
         public string PhotoUrl { get; set; }
+        public string Locale { get; set; }
         public string WelcomeMessage { get; set; }
+        public string Role { get; set; }
+        public Dictionary<string, string> Custom { get; set; }
     }
 }
