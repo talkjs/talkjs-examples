686c240a46bae125ed896d741bdad6d9e0471f69
DESKTOP-0N74O04\Aswin <aswinworkofficial@gmail.com>
2023-05-16 19:23:27 +0100

    break down of functions

--- rest-api/talkjs-html-panels-advanced/index.js
+++ rest-api/talkjs-html-panels-advanced/index.js
@@ -1,5 +1,6 @@
 const APP_ID = "YOUR_APP_ID";
 const TALKJS_URL = "https://api.talkjs.com/v1/";
+
 (async function () {
   await Talk.ready;
   let me = new Talk.User({
@@ -29,52 +30,27 @@ const TALKJS_URL = "https://api.talkjs.com/v1/";
   conversation.setParticipant(me);
   conversation.setParticipant(other);
 
-  let chatbox = talkSession.createChatbox();
+  const chatbox = talkSession.createChatbox();
   chatbox.select(conversation);
   chatbox.mount(document.getElementById("talkjs-container"));
 
-  let htmlPanel = await chatbox.createHtmlPanel({
+  const htmlPanel = await chatbox.createHtmlPanel({
     url: "./product-recommendations.html",
     height: 285,
     show: false,
   });
 
-  chatbox.onSendMessage(function (data) {
-    let messageText = data.message.text;
-    let conversationId = data.conversation.id;
-    console.log(conversationId);
-    getProducts(conversationId, messageText, htmlPanel);
+  chatbox.onSendMessage(async function (data) {
+    htmlPanel.isVisible() ? htmlPanel.hide() : htmlPanel.show();
+    const messageText = data.message.text;
+    const conversationId = data.conversation.id;
+    const productData = await getProducts(messageText, htmlPanel);
+    addProductsToHTMLPanel(productData, htmlPanel, conversationId);
   });
 })();
 
-//Function to post a success reply to the chat
-async function postSuccessReply(conversationId) {
-  const data = [
-    {
-      text: "Awesome. Here are our top recommendations! If you want to view other products, simply type the name of the product you want.",
-      sender: "654321",
-      type: "UserMessage",
-    },
-  ];
-  const talkJSURL = `${TALKJS_URL}${APP_ID}/conversations/${conversationId}/messages`;
-  const options = {
-    method: "POST",
-    headers: {
-      Authorization: "Bearer YOUR_SECRET_KEY",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  };
-
-  try {
-    const response = await fetch(talkJSURL, options);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
 //Function to get products from Sneaks API
-async function getProducts(conversationId, messageText, htmlPanel) {
+async function getProducts(messageText, htmlPanel) {
   if (htmlPanel.window.document.getElementById("row").hasChildNodes()) {
     htmlPanel.window.document.getElementById("row").innerHTML = "";
   }
@@ -89,51 +65,79 @@ async function getProducts(conversationId, messageText, htmlPanel) {
     const result = await response.json();
     const productData = result.slice(0, 10);
 
-    const row = htmlPanel.window.document.getElementById("row");
-
     await htmlPanel.DOMContentLoadedPromise;
-    await htmlPanel.windowLoadedPromise;
+    await htmlPanel.windowLoadedPromise;   
     
-    postSuccessReply(conversationId);
-    productData.forEach((product) => {
-      let cardDiv = htmlPanel.window.document.createElement("div");
-      cardDiv.classList.add("card");
+    return productData;
 
-      let brandName = htmlPanel.window.document.createElement("span");
-      brandName.classList.add("brand-name");
-      brandName.innerHTML = product.brand;
-
-      let cardImageLink = htmlPanel.window.document.createElement("a");
-      cardImageLink.href = product.resellLinks["stockX"];
-      cardImageLink.target = "_blank";
-      cardImageLink.innerHTML = product.shoeName;
-
-      let cardImage = htmlPanel.window.document.createElement("img");
-      cardImage.addEventListener("error", () => {
-        console.log(cardImage.src);
-        cardImage.src = "./static/no_image.png";
-      });
-      cardImage.src = product.thumbnail;
-      cardImage.classList.add("card-image");
-
-      let retailPrice = htmlPanel.window.document.createElement("span");
-      retailPrice.classList.add("price");
-      retailPrice.innerHTML = "Price: " + product.lowestResellPrice["stockX"];
-
-      cardDiv.appendChild(cardImage);
-      cardDiv.appendChild(brandName);
-      cardDiv.appendChild(retailPrice);
-      cardDiv.appendChild(cardImageLink);
-      row.appendChild(cardDiv);
-    });
-
-    const closeButton =
-      htmlPanel.window.document.getElementById("close-button");
-    closeButton.addEventListener("click", () => {
-      htmlPanel.hide();
-    });
-    htmlPanel.show();
   } catch (error) {
     console.log(error);
   }
+}
+
+//Function to post a success reply to the chat
+async function sendReply(conversationId) {
+  const data = [
+    {
+      text: "Awesome. Here are our top recommendations! If you want to view other products, simply type the name of the product you want.",
+      sender: "654321",
+      type: "UserMessage",
+    },
+  ];
+  const postReplyURL = `http://localhost:4000/postSuccessReply/${conversationId}`;
+  try {
+    const response = await fetch(postReplyURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//Function to add products to the HTML panel
+function addProductsToHTMLPanel(productData, htmlPanel, conversationId){
+  const row = htmlPanel.window.document.getElementById("row");
+
+  productData.forEach((product) => {
+    const cardDiv = htmlPanel.window.document.createElement("div");
+    cardDiv.classList.add("card");
+
+    const brandName = htmlPanel.window.document.createElement("span");
+    brandName.classList.add("brand-name");
+    brandName.innerHTML = product.brand;
+
+    const cardImageLink = htmlPanel.window.document.createElement("a");
+    cardImageLink.href = product.resellLinks["stockX"];
+    cardImageLink.target = "_blank";
+    cardImageLink.innerHTML = product.shoeName;
+
+    const cardImage = htmlPanel.window.document.createElement("img");
+    cardImage.addEventListener("error", () => {
+      console.log(cardImage.src);
+      cardImage.src = "./static/no_image.png";
+    });
+    cardImage.src = product.thumbnail;
+    cardImage.classList.add("card-image");
+
+    const retailPrice = htmlPanel.window.document.createElement("span");
+    retailPrice.classList.add("price");
+    retailPrice.innerHTML = "RRP: $" + product.lowestResellPrice["stockX"];
+
+    cardDiv.appendChild(cardImage);
+    cardDiv.appendChild(brandName);
+    cardDiv.appendChild(retailPrice);
+    cardDiv.appendChild(cardImageLink);
+    row.appendChild(cardDiv);
+  });
+  sendReply(conversationId);  
+  const closeButton = htmlPanel.window.document.getElementById("close-button");
+    closeButton.addEventListener("click", () => {
+      htmlPanel.hide();
+  });
+  htmlPanel.show();
 }
