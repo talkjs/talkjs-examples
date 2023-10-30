3fa9ebce65e4e542be585f7d6ce52c5418dd13ec
Aswin <aswinworkofficial@gmail.com>
2023-10-30 20:01:31 +0000

    removed server, other changes as per feedback

--- rest-api/add-gif-picker/talkjs-giphy-integration/index.js
+++ rest-api/add-gif-picker/talkjs-giphy-integration/index.js
@@ -2,37 +2,35 @@ import { GiphyFetch } from "@giphy/js-fetch-api";
 
 const gf = new GiphyFetch("<YOUR_GIPHY_API_KEY>");
 
-let conversationId = "";
-let currentUser = {};
+let conversation = {};
 
 const gifSearchForm = document.querySelector("form");
 
 gifSearchForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  const searchTerm = document.getElementById("searchTerm").value;
-  search(searchTerm);
+  (async () => {
+    const searchTerm = document.getElementById("searchTerm").value;
+    const gifResults = document.getElementById("gifSearchResults");
+    if(gifResults.innerHTML !== "") gifResults.innerHTML = "";
+    const result = await gf
+      .search(searchTerm, { sort: "popular", limit: 30 })
+      .then((response) => {
+        response.data.forEach((gif) => {
+          const cardImage = window.document.createElement("img");
+          cardImage.src = gif.images.fixed_width.url;
+          cardImage.classList.add("gif-image");
+          gifResults.appendChild(cardImage);
+        });
+      })
+      .catch((err) => console.error(`search`, err));
+  })();  
 });
 
-const search = async (searchTerm) => {
-  const gifResults = document.getElementById("gifSearchResults");
-  if(gifResults.innerHTML !== "") gifResults.innerHTML = "";
-  const result = await gf
-    .search(searchTerm, { sort: "popular", limit: 30 })
-    .then((response) => {
-      response.data.forEach((gif) => {
-        const cardImage = window.document.createElement("img");
-        cardImage.src = gif.images.fixed_width.url;
-        cardImage.classList.add("gif-image");
-        gifResults.appendChild(cardImage);
-      });
-    })
-    .catch((err) => console.error(`search`, err));
-};
 
 gifSearchForm.addEventListener("click", (event) => {
   if (event.target.tagName !== "IMG") return;
   else {
-    addGIFtoChat(conversationId, currentUser.id, event.target.src);
+    conversation.sendMessage(event.target.src, { custom: { gifAttachment: "true" }})
     const gifSearch = document.getElementById("gif-search");
     const gifResults = document.getElementById("gifSearchResults");
     gifResults.innerHTML = "";
@@ -40,26 +38,10 @@ gifSearchForm.addEventListener("click", (event) => {
   }
 });
 
-async function addGIFtoChat(conversationId, senderId, imageURL) {
-  const serverURL = `http://127.0.0.1:3000/addGIFtoChat`;
-  const data = {
-    conversationId: conversationId,
-    senderId: senderId,
-    imageURL: imageURL ? imageURL : undefined,
-  };
-  const response = await fetch(serverURL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  }).catch((err) => console.log(err));
-}
-
 (async () => {
   await Talk.ready;
   const me = new Talk.User({
-    id: "0001",
+    id: "0004",
     name: "Kirsten Doe",
     email: "kirsten.doe@example.com",
     photoUrl: "https://talkjs.com/images/avatar-1.jpg",
@@ -67,7 +49,7 @@ async function addGIFtoChat(conversationId, senderId, imageURL) {
   });
 
   const other = new Talk.User({
-    id: "0002",
+    id: "0005",
     name: "Mikaela Ross",
     email: "mikaela.ross@example.com",
     photoUrl: "https://talkjs.com/images/avatar-7.jpg",
@@ -75,21 +57,19 @@ async function addGIFtoChat(conversationId, senderId, imageURL) {
   });
   window.talkSession = new Talk.Session({
     appId: "<YOUR_APP_ID>",
-    me: other,
+    me: me,
   });
 
-  const conversation = talkSession.getOrCreateConversation(
+  conversation = talkSession.getOrCreateConversation(
     Talk.oneOnOneId(me, other)
   );
+
   conversation.setParticipant(me);
   conversation.setParticipant(other);
 
   const chatbox = talkSession.createChatbox();
   chatbox.select(conversation);
   chatbox.mount(document.getElementById("talkjs-container"));
-
-  conversationId = conversation.id;
-  currentUser = me;
 
   chatbox.onCustomConversationAction("pickGIF", (event) => {
     const gifSearch = document.getElementById("gif-search");
