4858ed822c8d374ba07d715acaaad1ab4d8b9eb3
Lucy Keer <lucy.keer@gmail.com>
2023-11-08 11:35:34 +0000

    Remove lastMessage

--- react/remote-work-demo/src/components/ConversationListItem.jsx
+++ react/remote-work-demo/src/components/ConversationListItem.jsx
@@ -1,8 +1,14 @@
 import ConversationImage from "./ConversationImage"
 
-const ConversationListItem = ({conversation, changeConversation, unreadMessages, currentConversation}) => {
-    
-    const unread = unreadMessages.find(item => item.lastMessage.conversation.id === conversation.id);
+const ConversationListItem = ({
+  conversation,
+  changeConversation,
+  unreadMessages,
+  currentConversation,
+}) => {
+  const unread = unreadMessages.find(
+    (item) => item.conversation.id === conversation.id
+  );
 
     return (
         <button onClick={() => changeConversation(conversation)} className={`flex flex-row items-center text-gray-400 hover:bg-gray-700 hover:text-white p-2 mx-4 rounded ${currentConversation.id === conversation.id ? 'bg-blue-500 hover:bg-blue-500 !text-white' : ''}`}>
