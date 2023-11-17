59ce3015770be0c9099ae90d25ae43ef4eb90a7b
shsaqib99 <85882036+shsaqib99@users.noreply.github.com>
2023-11-17 15:54:51 +0500

    Update php/routes/web.php
    Deleting default homepage route
    
    Co-authored-by: Lucy Keer <lucy@talkjs.com>

--- php/routes/web.php
+++ php/routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
