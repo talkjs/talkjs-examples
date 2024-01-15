6e2c1eec122590d28ed941c9847462f803160bf9
Victor Otieno <victor@slick.co.ke>
2024-01-15 21:13:00 +0300

    (Chore): Update ReactNavigationExample
    Signed-off-by: Victor Otieno <victor@slick.co.ke>

--- react-native/basic-example/ReactNavigationExample/index.tsx
+++ react-native/basic-example/ReactNavigationExample/index.tsx
@@ -1,9 +1,32 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
+import {
+  getNotificationHandler,
+  registerPushNotificationHandlers,
+} from '@talkjs/react-native';
+
 import App from './App';
 import {name as appName} from './app.json';
+
+registerPushNotificationHandlers(
+  {
+    channelId: 'com.reactnavigationexample.messages',
+    channelName: 'Messages',
+    badge: true,
+  },
+  {
+    sound: true,
+    badge: true,
+    alert: true,
+  },
+);
+const notificationHandler = getNotificationHandler();
+notificationHandler.onTokenRefresh((oldToken, newToken) => {
+  console.log(oldToken);
+  console.log(newToken);
+});
+
+notificationHandler.onNotificationPressed(notification => {
+  console.log(notification);
+});
 
 AppRegistry.registerComponent(appName, () => App);
