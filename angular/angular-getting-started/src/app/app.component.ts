172d9b44c5ea9834273500e110137e948fa30be3
Lucy Keer <lucy.keer@gmail.com>
2024-06-05 16:31:10 +0100

    Remove unused dependency

--- angular/angular-getting-started/src/app/app.component.ts
+++ angular/angular-getting-started/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
 import { TalkjsChatComponent } from "./talkjs-chat/talkjs-chat.component";
 
 @Component({
