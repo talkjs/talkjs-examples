f026838720255564d5cc6ecd352dbb2a521b224d
Lucy Keer <lucy.keer@gmail.com>
2024-06-07 12:43:25 +0100

    Example code for Vue getting started guide

---
+++ vue/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Chat from "./components/Chat.vue";
+
+const me = {
+  id: "sample_user_alice",
+  name: "Alice",
+  email: "nina@example.com",
+  photoUrl: "https://talkjs.com/new-web/avatar-7.jpg",
+  welcomeMessage: "Hi!",
+};
+</script>
+
+<template>
+  <header></header>
+
+  <main>
+    <Chat :currentUser="me" />
+  </main>
+</template>
