48a024f4ca4b8502f1023c5cdb2ba709505c3f36
Lucy Keer <lucy.keer@gmail.com>
2024-07-31 15:52:08 +0100

    Update settings.py

--- django/django_talkjs/django_talkjs/settings.py
+++ django/django_talkjs/django_talkjs/settings.py
@@ -117,8 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_URL = 'static/'
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
