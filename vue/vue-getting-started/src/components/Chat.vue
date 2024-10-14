b07decd9727f0f6e8dfba365e7fca8a203ed2c74
chris <csmeyns@users.noreply.github.com>
2024-10-14 14:21:40 +0200

    Update example to use ref, nextTick

--- vue/vue-getting-started/src/components/Chat.vue
+++ vue/vue-getting-started/src/components/Chat.vue
@@ -1,44 +1,50 @@
 //Chat.vue
 
 <template>
-  <div id="talkjs-container" style="width: 100%; height: 500px">
-      <i>Loading chat...</i>
+  <div ref="talkjsContainer" style="width: 100%; height: 500px">
+    <i>Loading chat...</i>
   </div>
 </template>
 
 <script setup>
-  import Talk from "talkjs";
-  import { onMounted } from 'vue';
+  import Talk from 'talkjs';
+  import { ref, onMounted, nextTick } from 'vue';
 
-  onMounted(() => {
-    Talk.ready.then(() => {
-      const me = new Talk.User({
-        id: 'nina',
-        name: 'Nina',
-        email: 'nina@example.com',
-        photoUrl: 'https://talkjs.com/new-web/avatar-7.jpg',
-        welcomeMessage: 'Hi!',
-      });
-      const session = new Talk.Session({
-        appId: '<APP_ID>',
-        me: me,
-      });
+  const talkjsContainer = ref(null);
 
-      const other = new Talk.User({
-        id: 'frank',
-        name: 'Frank',
-        email: 'frank@example.com',
-        photoUrl: 'https://talkjs.com/new-web/avatar-8.jpg',
-        welcomeMessage: 'Hey, how can I help?',
-      });
+  onMounted(async () => {
+    await Talk.ready;
+    
+    const me = new Talk.User({
+      id: 'nina',
+      name: 'Nina',
+      email: 'nina@example.com',
+      photoUrl: 'https://talkjs.com/new-web/avatar-7.jpg',
+      welcomeMessage: 'Hi!',
+    });
 
-      const conversation = session.getOrCreateConversation('new_conversation');
-      conversation.setParticipant(me);
-      conversation.setParticipant(other);
+    const session = new Talk.Session({
+      appId: '<APP_ID>',
+      me: me,
+    });
 
-      const chatbox = session.createChatbox();
-      chatbox.select(conversation);
-      chatbox.mount(document.getElementById('talkjs-container'));
-      });
+    const other = new Talk.User({
+      id: 'frank',
+      name: 'Frank',
+      email: 'frank@example.com',
+      photoUrl: 'https://talkjs.com/new-web/avatar-8.jpg',
+      welcomeMessage: 'Hey, how can I help?',
+    });
+
+    const conversation = session.getOrCreateConversation('new_conversation');
+    conversation.setParticipant(me);
+    conversation.setParticipant(other);
+
+    const chatbox = session.createChatbox();
+    chatbox.select(conversation);
+
+    await nextTick();
+
+    chatbox.mount(talkjsContainer.value);
   });
 </script>
