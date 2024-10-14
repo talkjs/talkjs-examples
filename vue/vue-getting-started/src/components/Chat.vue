6d2bb485f7ca662c1cf961f79cc7c0f62ade72ba
chris <csmeyns@users.noreply.github.com>
2024-10-14 14:34:26 +0200

    Revert "Update example to use ref, nextTick"
    This reverts commit b07decd9727f0f6e8dfba365e7fca8a203ed2c74.

--- vue/vue-getting-started/src/components/Chat.vue
+++ vue/vue-getting-started/src/components/Chat.vue
@@ -1,50 +1,44 @@
 //Chat.vue
 
 <template>
-  <div ref="talkjsContainer" style="width: 100%; height: 500px">
-    <i>Loading chat...</i>
+  <div id="talkjs-container" style="width: 100%; height: 500px">
+      <i>Loading chat...</i>
   </div>
 </template>
 
 <script setup>
-  import Talk from 'talkjs';
-  import { ref, onMounted, nextTick } from 'vue';
+  import Talk from "talkjs";
+  import { onMounted } from 'vue';
 
-  const talkjsContainer = ref(null);
+  onMounted(() => {
+    Talk.ready.then(() => {
+      const me = new Talk.User({
+        id: 'nina',
+        name: 'Nina',
+        email: 'nina@example.com',
+        photoUrl: 'https://talkjs.com/new-web/avatar-7.jpg',
+        welcomeMessage: 'Hi!',
+      });
+      const session = new Talk.Session({
+        appId: '<APP_ID>',
+        me: me,
+      });
 
-  onMounted(async () => {
-    await Talk.ready;
-    
-    const me = new Talk.User({
-      id: 'nina',
-      name: 'Nina',
-      email: 'nina@example.com',
-      photoUrl: 'https://talkjs.com/new-web/avatar-7.jpg',
-      welcomeMessage: 'Hi!',
-    });
+      const other = new Talk.User({
+        id: 'frank',
+        name: 'Frank',
+        email: 'frank@example.com',
+        photoUrl: 'https://talkjs.com/new-web/avatar-8.jpg',
+        welcomeMessage: 'Hey, how can I help?',
+      });
 
-    const session = new Talk.Session({
-      appId: '<APP_ID>',
-      me: me,
-    });
+      const conversation = session.getOrCreateConversation('new_conversation');
+      conversation.setParticipant(me);
+      conversation.setParticipant(other);
 
-    const other = new Talk.User({
-      id: 'frank',
-      name: 'Frank',
-      email: 'frank@example.com',
-      photoUrl: 'https://talkjs.com/new-web/avatar-8.jpg',
-      welcomeMessage: 'Hey, how can I help?',
-    });
-
-    const conversation = session.getOrCreateConversation('new_conversation');
-    conversation.setParticipant(me);
-    conversation.setParticipant(other);
-
-    const chatbox = session.createChatbox();
-    chatbox.select(conversation);
-
-    await nextTick();
-
-    chatbox.mount(talkjsContainer.value);
+      const chatbox = session.createChatbox();
+      chatbox.select(conversation);
+      chatbox.mount(document.getElementById('talkjs-container'));
+      });
   });
 </script>
