d6dce5cb4989e0bc2704136c33cadeccf004f46e
chris <csmeyns@users.noreply.github.com>
2024-10-16 11:12:18 +0200

    Use +page.svelte instead of index.svelte

---
+++ svelte/sveltekit/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import TalkjsChat from '../components/TalkjsChat.svelte'
+</script>
+  
+<main>
+    <h1>TalkJS chat example</h1>
+    <TalkjsChat />
+</main>
+
+<style>
+main {
+    text-align: center;
+    padding: 1em;
+    max-width: 800px;
+    margin: 0 auto;
+}
+</style>
