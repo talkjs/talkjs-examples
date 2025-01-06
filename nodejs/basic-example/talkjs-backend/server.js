14b64512fa78e62260fe339db97eb4518e7c9645
Lucy Keer <lucy.keer@gmail.com>
2025-01-06 10:52:21 +0000

    Remove unnecessary line

--- nodejs/basic-example/talkjs-backend/server.js
+++ nodejs/basic-example/talkjs-backend/server.js
@@ -11,7 +11,6 @@ const app = express();
 const port = 3000;
 app.use(cors());
 
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.post("/createUser", (req, res) => {
