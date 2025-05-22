3e4a79b25aa2b662fc443ccc09e0d84f11bd189a
Lucy Keer <lucy.keer@gmail.com>
2025-05-22 14:57:19 +0100

    Update server code to include placeholder message
    This is then replaced by a typing indicator in the theme

--- chatbot-integration/openai-chatgpt/server.js
+++ chatbot-integration/openai-chatgpt/server.js
@@ -16,13 +16,12 @@ async function getCompletion(messageHistory) {
     messages: messageHistory,
     model: "gpt-4o-mini",
   });
-
   const reply = completion.choices[0].message.content;
   return reply;
 }
 
-async function sendMessage(conversationId, text) {
-  return fetch(
+async function sendInitialMessage(conversationId) {
+  const response = await fetch(
     `https://api.talkjs.com/v1/${appId}/conversations/${conversationId}/messages`,
     {
       method: "POST",
@@ -32,11 +31,47 @@ async function sendMessage(conversationId, text) {
       },
       body: JSON.stringify([
         {
-          text: text,
+          text: "_Let me think for a bit..._", // Placeholder text that the theme will replace with a typing indicator
           sender: "chatbotExampleBot",
           type: "UserMessage",
+          custom: { isTyping: "true" },
         },
       ]),
+    }
+  );
+
+  // Return the message ID for later editing
+  const data = await response.json();
+  return data[0].id;
+}
+
+async function updateBotMessage(conversationId, messageId, text) {
+  return fetch(
+    `https://api.talkjs.com/v1/${appId}/conversations/${conversationId}/messages/${messageId}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${talkJSSecretKey}`,
+      },
+      body: JSON.stringify({
+        text: text,
+        custom: { isTyping: "false" },
+      }),
+    }
+  );
+}
+
+async function setUserAccess(conversationId, userId, access) {
+  return fetch(
+    `https://api.talkjs.com/v1/${appId}/conversations/${conversationId}/participants/${userId}`,
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${talkJSSecretKey}`,
+      },
+      body: JSON.stringify({ access: access }),
     }
   );
 }
@@ -58,18 +93,19 @@ app.post("/onMessageSent", async (req, res) => {
       },
     ];
   }
+
   const messageHistory = allMessageHistory[convId];
 
-  if (senderId == botId) {
-    // Bot message
-    messageHistory.push({ role: "assistant", content: messageText });
-  } else {
-    // User message
+  if (senderId != botId) {
     messageHistory.push({ role: "user", content: messageText });
-    getCompletion(messageHistory).then((reply) => sendMessage(convId, reply));
+    const messageId = await sendInitialMessage(convId);
+    await setUserAccess(convId, senderId, "Read");
+    const reply = await getCompletion(messageHistory);
+    await updateBotMessage(convId, messageId, reply);
+    messageHistory.push({ role: "assistant", content: reply });
+    await setUserAccess(convId, senderId, "ReadWrite");
   }
 
   res.status(200).end();
 });
 
-app.listen(3000, () => console.log("Server is up"));
